155a4731da0529dfc3864cf3d4259c98354f6b29
keerthana0179-web <keerthana0179@gmail.com>
2026-03-05 15:28:23 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [number, setnumber] = useState(0)
+   
+  const increment=()=>{
+    setnumber(number+1)
+  }
+  const decrement=()=>{
+    if(number>0) setnumber(number-1)
+  }
+  const reset=()=>{
+    setnumber(0)
+  }
+ return (
+  <div>
+    <h1>Counter app {number}</h1>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>
+  </div>
+    
+  )
+}
+
+export default App
